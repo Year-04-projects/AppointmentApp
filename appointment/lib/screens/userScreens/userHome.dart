7a3827ac959d40b8ee04000dd6ac80f9ae80872b
Rivindu2 <rivigith@gmail.com>
2023-03-12 13:53:24 +0530

    added user home page

--- appointment/lib/screens/userScreens/userHome.dart
+++ appointment/lib/screens/userScreens/userHome.dart
@@ -1,29 +1,83 @@
-
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../../services/auth_services.dart';
+import '../../utils/colors.dart';
 import '../authscreens/login.dart';
 
 class userHome extends StatefulWidget {
-  const userHome({super.key});
+  const userHome({Key? key}) : super(key: key);
 
   @override
   State<userHome> createState() => _userHomeState();
 }
 
 class _userHomeState extends State<userHome> {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
+  int _currentIndex = 0;
+  void onTabTap(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+//add screen widgets here
+  final List<Widget> _children = [
+    Center(child: Text('Home screen')),
+    Center(child: Text('2rd Screen')),
+    Center(
       child: ElevatedButton(
         onPressed: () {
           AuthServices().signOut();
-          Navigator.pushReplacement(
-              context, MaterialPageRoute(builder: (context) => login()));
+          
         },
-        child: Text('userSignout'),
+        child: Text('User Signout'),
+      ),
+    ),
+  ];
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: secondaryColor,
+          title: Row(
+            children: [
+              Image.asset(
+                'assets/logo.png',
+                width: 48,
+              ),
+              SizedBox(width: 10),
+              Text(
+                'Medicare',
+                style: GoogleFonts.urbanist(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: -0.1,
+                  color: primaryColor
+                ),
+              ),
+            ],
+          )),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: onTabTap,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.outlet_sharp),
+            label: 'Other',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
       ),
     );
-    ;
   }
 }
