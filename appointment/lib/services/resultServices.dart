79bb6b111fbfe880e8966b08b38c467373857379
nirasha jayasiri <nirashajayasiri@gmail.com>
2023-03-24 18:10:15 +0530

    results services modified

--- appointment/lib/services/resultServices.dart
+++ appointment/lib/services/resultServices.dart
@@ -29,9 +29,53 @@ class ResultService {
     return res;
   }
 
+  // add results
+
   static Stream<QuerySnapshot> readResults() {
     CollectionReference labResultsCollection = _collection;
 
     return labResultsCollection.snapshots();
   }
+
+  // view results
+
+  static Future<LabResponse> updateLabResult(
+      {required String name, required int age, required String id}) async {
+    LabResponse labRes = LabResponse();
+    DocumentReference docRef = _collection.doc(id);
+
+    Map<String, dynamic> data = <String, dynamic>{
+      "Result name": name,
+      "age": age,
+    };
+
+    await docRef.update(data).whenComplete(() {
+      labRes.code = 200;
+      labRes.message = "Success";
+    }).catchError((e) {
+      labRes.code = 500;
+      labRes.message = e;
+    });
+
+    return labRes;
+  }
+
+  // update results
+
+  static Future<LabResponse> deleteLabResult({required String docId}) async {
+    LabResponse labRes = LabResponse();
+    DocumentReference docRef = _collection.doc(docId);
+
+    await docRef.delete().whenComplete(() {
+      labRes.code = 200;
+      labRes.message = "Deleted!";
+    }).catchError((e) {
+      labRes.code = 500;
+      labRes.message = e;
+    });
+
+    return labRes;
+  }
+
+  // delete result
 }
